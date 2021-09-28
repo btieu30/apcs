3906fa6f97ae1f30f90c0853c8e6609bd24ec58e
Brianna Tieu <btieu30@stuy.edu>
2021-09-28 00:51:39 -0400

    created 01 folder

---
+++ pre/01/Hello.java
@@ -0,0 +1,11 @@
+"Brianna Tieu
+APCS
+HW: Pre-Work 01 -- AP Computer Science Pre-Work #01
+2021-09-27"
+
+public class Hello{
+  public static void main(String[] args){
+  System.out.println("Hello world!");
+  }
+}
+
