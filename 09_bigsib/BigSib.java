9871f8664bcae4f690cd6834fa9aafb7ca807719
Brianna Tieu <btieu30@stuy.edu>
2021-10-02 21:11:57 -0400

    adding BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,2 +1,13 @@
 /* Anything Goes: 
+Brianna (Pepe), William (CS50DDB), Brian (Dolphin)
+
+Discoveries and Unresolved Questions in Greet.java!
 */
+
+public class BigSib {
+	public static void greet(String x) {
+		System.out.println( "Why, hello there, " + x + ". How do you do?");
+	}
+}
+
+
