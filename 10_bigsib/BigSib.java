710ca1145fa47167eb44baaa78efc246d6322562
Brianna Tieu <btieu30@stuy.edu>
2021-10-04 21:21:47 -0400

    adding HW 10 files

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+/* Anything Goes: 
+Brianna (Pepe), William (CS5ODDB), Brian (Dolphin)
+
+Discoveries and Unresolved Q's in Greet.java
+*/
+
+public class BigSib {
+	public static String greet(String x) {
+		String s = ("Why, hello there, " + x + ". How do you do?");
+		return s;
+	}
+}
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,18 @@
+/* Anything Goes:
+Brianna (Pepe), William (CS50DDB), Brian (Dolphin)
+
+Discoveries:
+1) When we change the "void" to what we what the method to output, in this case, it is a string.
+
+Unresolved Questions:
+1) Is it more organized to have a method return and only call it to print an output in the main method?
+
+*/
+
+public class Greet {
+	public static void main(String[] args){
+		System.out.println( BigSib.greet("Pepe") );
+		System.out.println( BigSib.greet("CS50DDB") );
+		System.out.println( BigSib.greet("Dolphin") );
+	}
+}
