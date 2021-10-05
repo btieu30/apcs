0e0508a4d9f926d91e8ac9bc6c233fff587148ac
Brianna Tieu <btieu30@stuy.edu>
2021-10-04 21:13:38 -0400

    heading changes

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,13 +1,15 @@
 /* Anything Goes: 
 Brianna (Pepe), William (CS50DDB), Brian (Dolphin)
+APCS
+HW 09 - Wbat are BigSIbs Good For?
+2021-10-02
 
 Discoveries and Unresolved Questions in Greet.java!
 */
 
 public class BigSib {
-	public static void greet(String x) {
+	public static void  greet(String x) {
 		System.out.println( "Why, hello there, " + x + ". How do you do?");
 	}
 }
-
 
